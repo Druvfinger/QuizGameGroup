fe21eed7a2ebdf1b1f72cc0a58367f36fc88f119
Robin lundell <robin.lundell@hotmail.com>
2022-11-22 12:16:28 +0100

    Properties function

--- src/ServerSideGame.java
+++ src/ServerSideGame.java
@@ -1,23 +1,54 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 public class ServerSideGame {
 
     ServerSidePlayer currentPlayer;
 
-    public boolean isWinner(){
-        if (isLastRound() && isInTheLead());{
+
+    public boolean isWinner() {
+        if (isLastRound() && isInTheLead()) ;
+        {
             return true;
         }
     }
-    public boolean isLastRound(){
+
+    public boolean isLastRound() {
         return false;
     }
-    public boolean isInTheLead(){
+
+    public boolean isInTheLead() {
         return false;
     }
-    public void chooseCategory(){
+
+    public void chooseCategory() {
 
     }
-    public String getRandomQuestionFromCategory(){
+
+    public String getRandomQuestionFromCategory() {
         return null;
     }
+
+    public int getNumberOfRounds() {
+        try (InputStream input = new FileInputStream("src/Settings.properties")) {
+            Properties prop = new Properties();
+            return Integer.parseInt(prop.getProperty("numberOfRounds", "3"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getNumberOfQuestions() {
+        try (InputStream input = new FileInputStream("src/Settings.properties")) {
+            Properties prop = new Properties();
+            return Integer.parseInt(prop.getProperty("numberOfQuestions", "3"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
 
