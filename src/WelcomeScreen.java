1aab69a54b53e0ae4249ae2d08088c4353ce9e52
Robin lundell <robin.lundell@hotmail.com>
2022-11-28 14:11:42 +0100

    Changes by jbirg

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -4,6 +4,8 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class WelcomeScreen extends JFrame {
 
